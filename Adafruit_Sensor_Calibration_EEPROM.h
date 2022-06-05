0b2f6838b66f4aedebb5011de53da0c94e52a1eb
RedshiftVelocities <43499473+RedshiftVelocities@users.noreply.github.com>
2022-06-05 15:24:52 -0600

    Initial commit

---
+++ Adafruit_Sensor_Calibration_EEPROM.h
@@ -0,0 +1,24 @@
+#ifndef __ADAFRUIT_SENSOR_CALIBRATION_EEPROM__
+#define __ADAFRUIT_SENSOR_CALIBRATION_EEPROM__
+
+#include "Adafruit_Sensor_Calibration.h"
+
+#include <FlashAsEEPROM.h>
+
+#define EEPROM_CAL_SIZE 68
+
+/**!  @brief Class for managing storing calibration in internal EEPROM memory
+ * **/
+class Adafruit_Sensor_Calibration_EEPROM : public Adafruit_Sensor_Calibration {
+public:
+  bool begin(uint8_t eeprom_addr = 60);
+
+  bool saveCalibration(void);
+  bool loadCalibration(void);
+  bool printSavedCalibration(void);
+
+private:
+  uint16_t ee_addr = 0;
+};
+
+#endif // include once
